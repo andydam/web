40f25d9ddea1fcacc06dc2bcc20bcf2cb2485df0
Andy Dam <andy@andydam.com>
2018-02-09 21:55:19 -0800

    test: add tests for basic path

--- methods/method.js
+++ methods/method.js
@@ -1,13 +1,27 @@
 module.exports = class Method {
   constructor() {
-    this.handlers = [];
+    this.handlers = {};
   }
-  method(handler) {
-    this.handlers.push(handler);
+  method(path, ...handler) {
+    if (typeof path === 'string') {
+      if (this.handlers[path]) {
+        this.handlers[path].push(...handler);
+      } else {
+        this.handlers[path] = [...handler];
+      }
+    } else if (typeof path === 'function') {
+      if (this.handlers['/']) {
+        this.handlers['/'].push(path, ...handler);
+      } else {
+        this.handlers['/'] = [path, ...handler];
+      }
+    }
   }
   async handler(request, response) {
-    for (let i = 0; i < this.handlers.length; i += 1) {
-      await this.handlers[i](request, response);
+    if (this.handlers[request.url]) {
+      for (let i = 0; i < this.handlers[request.url].length; i += 1) {
+        await this.handlers[request.url][i](request, response);
+      }
     }
   }
 };
