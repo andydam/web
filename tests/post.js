93773ce7d0f1af32c6cc449998807e89c9b7d673
Andy Dam <andy@andydam.com>
2018-02-09 16:40:07 -0800

    test: add tests for post method

---
+++ tests/post.js
@@ -0,0 +1,48 @@
+const { expect } = require('chai');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const web = require('../index');
+
+const server = web();
+
+chai.use(chaiHttp);
+
+describe('POST method', () => {
+  before((done) => {
+    server.listen();
+    done();
+  });
+  it('should add a handler for POST requests', (done) => {
+    const testHandler = (request, response) => {
+      response.writeHead(200);
+      response.end(request.body);
+    };
+    server.post(testHandler);
+    expect(server._postHandlers).to.have.lengthOf.at.least(1);
+    expect(server._postHandlers[0]).to.equal(testHandler);
+    done();
+  });
+  it('should use the added handler for POST requests', (done) => {
+    chai
+      .request(server)
+      .post('/')
+      .send('test')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.text).to.equal('test');
+        done();
+      });
+  });
+  it('should parse incoming data and set it to request.body', (done) => {
+    chai
+      .request(server)
+      .post('/')
+      .send('testData')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.text).to.equal('testData');
+        done();
+      });
+  });
+});
