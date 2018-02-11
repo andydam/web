57c13a50df851abf7cc5ba662b6bc7386b8a6324
Andy Dam <andy@andydam.com>
2018-02-10 19:18:50 -0800

    feat: add support for basic url param

--- methods/method.js
+++ methods/method.js
@@ -18,9 +18,30 @@ module.exports = class Method {
     }
   }
   async handler(request, response) {
-    if (this.handlers[request.url]) {
-      for (let i = 0; i < this.handlers[request.url].length; i += 1) {
-        await this.handlers[request.url][i](request, response);
+    for (
+      let i = 0, handler = Object.keys(this.handlers);
+      i < handler.length;
+      i += 1
+    ) {
+      if (handler[i].indexOf(':') > -1) {
+        const path = handler[i].replace(/:.+$/, '');
+        const paramName = handler[i].replace(/^.+:/, '');
+
+        if (request.url.lastIndexOf(path, 0) === 0) {
+          const paramValue = request.url.substr(path.length);
+          const requestWithParam = {
+            ...request,
+            params: { [paramName]: paramValue },
+          };
+
+          for (let o = 0; o < this.handlers[handler[i]].length; o += 1) {
+            await this.handlers[handler[i]][o](requestWithParam, response);
+          }
+        }
+      } else if (handler[i] === request.url) {
+        for (let o = 0; o < this.handlers[handler[i]].length; o += 1) {
+          await this.handlers[handler[i]][o](request, response);
+        }
       }
     }
   }
