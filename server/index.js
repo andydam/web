d6b62ca77382d6ab6abb6fc0f1ebc8b6b0f65238
Andy Dam <andy@andydam.com>
2018-02-11 17:33:45 -0800

    feat: add use method to add handlers to be applied to all http methods

--- server/index.js
+++ server/index.js
@@ -6,8 +6,10 @@ const web = () => {
   const httpRouter = router();
 
   const httpServer = Object.assign(http.createServer(httpRouter), {
+    use: (...args) => httpRouter.use(...args),
     get: (...args) => httpRouter.get(...args),
     post: (...args) => httpRouter.post(...args),
+    _useHandlers: httpRouter._useHandlers,
     _getHandlers: httpRouter._getHandlers,
     _postHandlers: httpRouter._postHandlers,
   });
